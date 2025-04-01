9ca2c4088161c08eec78b73e8098c65c538265ae
diego <marcin.plawik3@wp.pl>
2025-04-02 01:15:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.CONFIG = {
     DISCORD_WEBHOOK: "https://discord.com/api/webhooks/1333931409148088414/sV-jn_QbNtHoLJ3wjixqXVg9tb_tdvkXcgEMlGeNCvrfFJJULdK9_j4nvEbsxzfLo9nT",
-    IPAPI_KEY: "TWÓJ_KLUCZ_API" // Opcjonalnie (zarejestruj się na ipapi.co)
+    IPAPI_KEY: "TWÓJ_KLUCZ_IPAPI", // Rejestracja na ipapi.co
+    VPNAPI_KEY: "TWÓJ_KLUCZ_VPN"    // Rejestracja na vpnapi.io
 };
